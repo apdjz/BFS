794cbd0e76bb87f29afc2b9cb0461fdef4c1b302
Andrew Ziegler <14apdjz@gmail.com>
2021-02-18 19:25:08 -0500

    Update format

--- main.js
+++ main.js
@@ -6,40 +6,40 @@
 */
 
 
-let levelOrder = function(root) {
-  //checks if root exists
-  if (!root) return [];
-  //Note ** arrays as queues are simple but inefficient
-  //Initialize array (FIFO)
-  var arr = [];
-  arr.push(root);
-  
-  let result = [];
-  
-  //outter loop run until arr is empty
-  while (arr.length > 0) {
-    let row = [];
-    // keep track of when row ends
-    let rowSize = arr.length;
-      
-    while (rowSize > 0) {
-      //take out bottom of the arr
-      let curr = arr.shift();
-      
-      //check if there are children
-      if (curr.left !== null) {
-        arr.push(curr.left);
-      }
-      if (curr.right !== null) {
-        arr.push(curr.right);
-      }
-            
+let levelOrder = function (root) {
+    //checks if root exists
+    if (!root) return [];
+    //Note ** arrays as queues are simple but inefficient
+    //Initialize array (FIFO)
+    var arr = [];
+    arr.push(root);
+
+    let result = [];
+
+    //outter loop run until arr is empty
+    while (arr.length > 0) {
+        let row = [];
+        // keep track of when row ends
+        let rowSize = arr.length;
+
+        while (rowSize > 0) {
+            //take out bottom of the arr
+            let curr = arr.shift();
+
+            //check if there are children
+            if (curr.left !== null) {
+                arr.push(curr.left);
+            }
+            if (curr.right !== null) {
+                arr.push(curr.right);
+            }
+
             row.push(curr.val);
             rowSize--;
         }
-        
+
         result.push(row);
     }
-    
+
     return result;
 };
