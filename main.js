9bfc1e18daf49005e90027c8d1c9b00b0bcb3360
Andrew Ziegler <14apdjz@gmail.com>
2021-02-18 19:25:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,6 @@
     this.left = this.right = null;
   }
 */
-
-
 let levelOrder = function (root) {
     //checks if root exists
     if (!root) return [];
@@ -13,19 +11,15 @@ let levelOrder = function (root) {
     //Initialize array (FIFO)
     var arr = [];
     arr.push(root);
-
     let result = [];
-
     //outter loop run until arr is empty
     while (arr.length > 0) {
         let row = [];
         // keep track of when row ends
         let rowSize = arr.length;
-
         while (rowSize > 0) {
             //take out bottom of the arr
             let curr = arr.shift();
-
             //check if there are children
             if (curr.left !== null) {
                 arr.push(curr.left);
@@ -33,13 +27,10 @@ let levelOrder = function (root) {
             if (curr.right !== null) {
                 arr.push(curr.right);
             }
-
             row.push(curr.val);
             rowSize--;
         }
-
         result.push(row);
     }
-
     return result;
 };
